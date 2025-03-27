729640af177f456bf4cc55007c8b7c4c34875dcb
sin <sin@hekr.cloud>
2025-03-27 21:11:01 +0800

    update

---
+++ app/agents/proptemts/unclear_propmt.py
@@ -0,0 +1,24 @@
+UnClearTemplate = """
+You are Tikee AI, an 
+intelligent and friendly crypto wallet assistant. 
+Your primary function is to assist users with their cryptocurrency-related needs, including wallet functionality, transaction guidance, security best practices, 
+and general crypto knowledge.
+
+user:{input}
+
+【返回格式】
+仅返回 JSON 数据，不要附加任何其他文本（注意：布尔值必须为 true 或 false，不使用引号）：
+当前语言:{langguage}
+
+json
+{{"data": {{
+    "description": "系统生成的自然语言回复内容(需要根据当前的语言进行翻译 如果是英文则翻译为英文)",
+    "state": "{{}}",
+    "form": {{}},
+    "missFields":[
+    {{}}
+],
+    "DxTransActionDetail": {{}}
+}}}}
+在上面的json结果中 只要涉及到自然语言的 必须按照 {langguage} 进行翻译即可
+"""
--- app/agents/tasks/handle_unclear.py
+++ app/agents/tasks/handle_unclear.py
@@ -1,5 +1,5 @@
 # 深度搜索分析
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 from langchain_core.prompts import PromptTemplate
 
 from app.agents.lib.session.sessionManager import dict_manager
@@ -7,33 +7,19 @@ from app.agents.schemas import AgentState
 from app.agents.lib.llm.llm import LLMFactory
 from app.agents.tools import GetWrapResponse
 from app.config import settings
-
+from app.agents.lib.redisManger.redisManager import redis_dict_manager
+from app.agents.proptemts.unclear_propmt import  UnClearTemplate
 
 async def unclear_task(state: AgentState) -> AgentState:
-    system_message = """
-    You are Tikee AI, a helpful crypto wallet assistant.
-
-    The user's request doesn't match any specific crypto transaction or research task.
-
-    Provide general assistance about cryptocurrency topics, wallet functionality, or redirect them to specific features if appropriate.
-
-    Always respond in a friendly, helpful tone.
-    """
-    print("222")
+    system_message = UnClearTemplate
     llm = LLMFactory.getOpenAI(open_key=settings.OPENAI_API_KEY, url=settings.OPENAI_API_BASE_URL)
-    prompt = PromptTemplate.from_template(system_message)
-    chain = prompt | llm | StrOutputParser()
+    prompt = PromptTemplate(
+        template=system_message,
+        input_variables=["input", "langguage"],
+    )
+    chain = prompt | llm | JsonOutputParser()
     # 将 state.user_input 封装成字典
-    input_data = {"input": state.user_input} if state.user_input else {}
+    input_data = {"input": state.user_input, "language": state.language}
     result = await chain.ainvoke(input_data)
     print("进入无法匹配模式")
-    response = GetWrapResponse(
-        data=result,
-        history=dict_manager.get(state.session_id).get("history"),  # 历史信息
-        system_response="",
-        missfield="",
-        description="",
-        is_completed=False,
-        detected_intent=state.detected_intent.value
-    )
-    return state.copy(update={"result": response})
+    return state.copy(update={"result": result})
