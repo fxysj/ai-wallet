ffa1581a9d8adb4a9651b28488f4ae3229ad6a2f
sin <sin@sindeMacBook-Air.local>
2025-04-02 16:34:31 +0800

    update

---
+++ app/agents/proptemts/news_form_prompt.py
@@ -0,0 +1,50 @@
+#新闻资讯提示词模版
+NEWS_TEMPLATE = """
+你是一个专业的区块链新闻资讯查询助手，任务是帮助用户完善转账信息。请依据以下要求处理对话历史和用户最新输入：
+
+【目标】
+- 更新已有数据（若用户提供有效新信息则覆盖，否则保留原数据）。
+- 检查当前数据中缺失的必填字段。
+- 返回完整的表单信息，并生成自然流畅的回复，指导用户补充信息。
+
+【需要收集的字段】（严格遵循字段名称和格式）：
+- timeframe:  时间类型(e.g. "daily", "weekly", "monthly")
+【输入内容】
+- 当前对话历史：{history}
+- 用户最新输入：{input}
+- 当前数据：{current_data}
+
+【任务要求】
+1. 根据用户输入更新数据字段，保留已有有效信息；
+2. 检查并列出所有缺失字段；
+3. 无论用户更新或修改数据，都返回完整填充的表单信息；
+4. 当用户提出“xx错误”或“我要修改xx不对”时，识别具体字段进行更新；
+5. 生成自然流畅的回复，帮助用户了解需要补充的信息。
+
+【State 定义】  
+- `NEWSLETTER_TASK_DISPLAY_NEWSLETTER`：字段完整新增展示。  
+- `NEWSLETTER_TASK_NEED_MORE_INFO`：字段缺失，需要用户补充信息。  
+【返回格式】
+仅返回 JSON 数据，不要附加任何其他文本（注意：布尔值必须为 true 或 false，不使用引号）：
+当前语言:{langguage}
+
+json
+{{"data": {{
+    "description": "系统生成的自然语言回复内容(需要根据当前的语言进行翻译 如果是英文则翻译为英文)",
+    "state": "{{
+        'NEWSLETTER_TASK_DISPLAY_NEWSLETTER' if 所有字段完整 else 'NEWSLETTER_TASK_NEED_MORE_INFO'
+    }}",
+    ”timestamp“：”Python 返回的UTC的时间戳的格式",
+    "form": {{
+      "timeframe": "更新后的时间类型(e.g. "daily", "weekly", "monthly")",
+    }},
+    "missFields": [
+    {{
+        "name": "缺失字段名称",
+        "description": "字段描述 （需要根据当前langguage 进行翻译)"
+    }}
+],
+    "newsletter": {{}}
+}}}}
+在上面的json结果中 只要涉及到自然语言的 必须按照 {langguage} 进行翻译即可
+"""
--- app/agents/tasks/news_task.py
+++ app/agents/tasks/news_task.py
@@ -1,4 +1,35 @@
 #新闻资讯任务处理器
+import time
+
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
+
+from app.agents.lib.llm.llm import LLMFactory
+from app.agents.proptemts.news_form_prompt import NEWS_TEMPLATE
 from app.agents.schemas import AgentState
 async def news_task(state: AgentState) -> AgentState:
-    return state.copy(update={"task_result": "news_task 处理完成", "is_signed": True})
+    print("send_task")
+    print("DEBUG - attached_data 类型:", type(state.attached_data))
+    print("DEBUG - attached_data 内容:", state.attached_data)
+    prompt = PromptTemplate(
+        template=NEWS_TEMPLATE,
+        input_variables=["current_data", "history", "input", "langguage"],
+    )
+    llm = LLMFactory.getDefaultOPENAI()
+    chain = prompt | llm | JsonOutputParser()
+    # 调用链处理用户最新输入
+    chain_response = chain.invoke({
+        "current_data": str(state.attached_data),
+        "history": state.history,
+        "input": state.user_input,
+        "langguage": state.langguage
+    })
+    response_data = chain_response
+    data = response_data.get("data")
+    data["intent"] = state.detected_intent.value
+    # 使用 time 模块获取当前时间戳
+    timestamp_time = time.time()
+    print("使用 time 模块获取的 UTC 时间戳:", timestamp_time)
+    data["timestamp"] = state.attached_data.get("timestamp", timestamp_time)
+
+    return state.copy(update={"result": data})
