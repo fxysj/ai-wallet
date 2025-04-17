2cd00b4bda1d7e7e98049517ded49081c9503df1
sin <sin@hekr.cloud>
2025-04-17 21:29:53 +0800

    update

--- app/main.py
+++ app/main.py
@@ -1,15 +1,32 @@
 # FastAPI 入口
+from contextlib import asynccontextmanager
+
 import uvicorn
 from app.api.chat_api import router as chat_router
 from app.api.exceptions.register import register_exception_handlers
 from app.api.middleware.agentState_middleware import AgentStateSaveMiddleware
 from app.api.middleware.cores_middleware import setup_cors_middleware
 from fastapi import FastAPI
+from app.db.database import engine, Base
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # ✅ 在服务启动时初始化数据库表
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        print("✅ 数据库表结构已初始化")
 
+    yield  # 应用运行期间
+
+    # 👇 可选：在服务关闭前执行清理
+    print("🛑 FastAPI 应用即将关闭")
+
+# 绑定 lifespan
 app = FastAPI(title="区块链智能助手 Pro",
               description="提供转账、查询余额等功能",
-              version="1.0.0")
+              version="1.0.0",lifespan=lifespan)
+
+
 #设置请求后响应日志stateAgent采集
 app.add_middleware(AgentStateSaveMiddleware)
 #设置跨域
@@ -18,6 +35,7 @@ app = setup_cors_middleware(app)
 app.include_router(chat_router, prefix="/api/v1")
 #注册异常拦截器
 register_exception_handlers(app)
+
 
 
 # 启动服务
